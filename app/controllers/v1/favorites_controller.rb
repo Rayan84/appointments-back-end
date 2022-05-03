91b37d1d8d59da0b5e470292cb4fbd617a337f94
Rayan84 <rayan_84@live.com>
2022-05-03 18:10:12 +0200

    Add favorites controllers

--- app/controllers/v1/favorites_controller.rb
+++ app/controllers/v1/favorites_controller.rb
@@ -2,7 +2,20 @@ class V1::FavoritesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @favorites = Favorite.where(user_id: params[user.id])
+    @favorites = Favorite.all.where(user_id: params[:user.id])
     render json: @favorites
+  end
+
+  def create
+    @favorite = Favorite.create(user_params)
+  end
+
+  def delete
+    @favorite = Favorite.where(user_params).delete
+  end
+
+  private
+  def user_params
+    params.require(:favorite).permit(:user_id, :item_id)
   end
 end
