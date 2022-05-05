b8acd55c6ec58b056d4e4bff2b595ba7d65966b5
Rayan84 <rayan_84@live.com>
2022-05-05 12:39:46 +0200

    Add validations and associations

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,2 +1,7 @@
 class Favorite < ApplicationRecord
+  belongs_to :user
+  belongs_to :item
+
+  validates :user_id, presences: true
+  validates :item_id, presences: true
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  has_many :favorites
+
+  validates :name, presences: true, length: { in: 3..15 }
+  validates :photo, presences: true
+  validates :price, presences: true, numericality: { greater_than: 0 }
+  validates :description, presences: true 
 end
